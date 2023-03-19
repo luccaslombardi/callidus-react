db5a305156909a7cd6eba856ffc74edbe4af3abc
luccaslombardi <luccas.lombardi@hotmail.com>
2023-03-19 13:48:49 -0300

    feat: New Layout Under Contruction

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import callidusLogo from './assets/logo-callidus.png'
 import './App.css'
+import './fonts/AltoneTrial-Bold.ttf'
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
         <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={callidusLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className="titleContainer">
+        <div className="titleDiv">
+          <h1>callidus</h1>
+          <h3>comunicação</h3>
+        </div>
+
       </div>
+
+
+      <span className="read-the-docs">
+        Site em Construção
+      </span>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Clique no Logo da Callidus para saber mais...
       </p>
     </div>
   )
