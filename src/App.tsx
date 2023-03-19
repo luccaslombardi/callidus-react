5a7499fc746f7590217ac2ad0178163f24d14e68
luccaslombardi <luccas.lombardi@hotmail.com>
2023-03-19 18:16:24 -0300

    primeiro commit da branch

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import callidusLogo from './assets/logo-callidus.png'
+import './App.css'
+import './fonts/AltoneTrial-Bold.ttf'
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <div>
+        <a href="https://www.instagram.com/calliduscomunicacao/" target="_blank">
+          <img src={callidusLogo} className="logo react" alt="Callidus Instagram" />
+        </a>
+      </div>
+      <div className="titleContainer">
+        <div className="titleDiv">
+          <h1>callidus</h1>
+          <h3>comunicação</h3>
+        </div>
+
+      </div>
+
+
+      <span>
+        Site em Construção
+      </span>
+      <p className="read-the-docs">
+        Clique no logo para saber mais...
+      </p>
+    </div>
+  )
+}
+
+export default App
